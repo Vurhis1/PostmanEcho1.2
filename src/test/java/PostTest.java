343b31367edc8a8d463aa208c9fecbbdff6b75e9
Vurhis1 <denraimond@gmail.ru>
2022-10-18 14:08:24 +0300

    First commit

---
+++ src/test/java/PostTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static io.restassured.RestAssured.*;
+import static org.apache.http.client.utils.DateUtils.GMT;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTest {
+
+  @Test
+  void shouldReturnDemoAccounts() {
+
+      given()
+              .baseUri("https://postman-echo.com")
+              .contentType("text/plain; charset=UTF-8")
+              .body("some data")
+              .when()
+              .post("/post")
+              .then()
+              .statusCode(200)
+              .body("balance".);
+
+}
+}
